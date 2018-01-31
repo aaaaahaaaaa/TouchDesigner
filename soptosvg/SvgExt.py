1e9abecfaed8df00423fcffc2c8d1e14f92dff10
Guillaume Onfroy <guillaume.onfroy@gmail.com>
2018-01-31 02:10:20 +0100

    Initial commit

---
+++ soptosvg/SvgExt.py
@@ -0,0 +1,189 @@
+"""
+Copyright © 2018 Guillaume Onfroy <guillaume.onfroy@gmail.com>
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+"""
+
+import svgwrite
+import time
+import datetime
+import numpy
+from array import array
+from TDStoreTools import StorageManager
+TDF = op.TDModules.mod.TDFunctions
+
+class SvgExt:
+
+	def __init__(self, ownerComp):
+		self.ownerComp = ownerComp
+		self.initParameters()
+		self.loadParameters()
+		print('SvgExt initialized')
+
+
+	def initParameters(self):
+		svgPage = self.ownerComp.appendCustomPage('SVG')
+
+		svgPage.appendSOP('Sop', label='SOP', order='0')
+		projectionMenu = svgPage.appendMenu('Projection', label='Projection', order='1')[0]
+		projectionMenu.menuNames = ['Camera', 'Offset']
+		projectionMenu.menuLabels = ['Camera\'s matrix', '2D Offset']
+		svgPage.appendCOMP('Camera', label='Camera', order='2')
+		svgPage.appendXY('Offset', label='Offset', order='3')
+		svgPage.appendToggle('Polylineonly', label='Polyline Only', order='4')
+
+		unitMenu = svgPage.appendMenu('Unit', label='Unit', order='5')[0]
+		unitMenu.startSection = True
+		unitMenu.menuNames = ['mm', 'px']
+		unitMenu.menuLabels = ['mm', 'px']
+		svgPage.appendWH('Canva', label='Canva Size', order='6')
+		margin = svgPage.appendInt('Margin', label='Margin', order='7')[0]
+		margin.normMax = 50
+		svgPage.appendToggle('Scaletofit', label='Scale To Fit & Center', order='8')
+
+		svgPage.appendFolder('Folder', label='Folder', order='9')[0].startSection = True
+		svgPage.appendStr('Filename', label='File Name', order='10')
+		suffixeMenu = svgPage.appendMenu('Suffixe', label='Suffixe', order='11')[0]
+		suffixeMenu.menuNames = ['None', 'Timestamp']
+		suffixeMenu.menuLabels = ['None', 'Timestamp']
+		svgPage.appendPulse('Savesvg', label='Save SVG', order='12')
+
+
+	def loadParameters(self):
+		self.sop = op(self.ownerComp.par.Sop)
+		self.projection = self.ownerComp.par.Projection
+		self.camera = op(self.ownerComp.par.Camera)
+		self.viewMatrix = self.camera.worldTransform
+		self.viewMatrix.invert()
+		self.projectionMatrix = self.camera.projection(1, 1)
+		self.offsetX = self.ownerComp.par.Offsetx
+		self.offsetY = self.ownerComp.par.Offsety
+		self.polylineOnly = self.ownerComp.par.Polylineonly
+		self.unit = self.ownerComp.par.Unit
+		self.scaleToFit = self.ownerComp.par.Scaletofit
+		self.canvaW = self.ownerComp.par.Canvaw
+		self.canvaH = self.ownerComp.par.Canvah
+		self.margin = self.ownerComp.par.Margin
+		self.folder = self.ownerComp.par.Folder
+		self.filename = self.ownerComp.par.Filename
+		self.suffixe = self.ownerComp.par.Suffixe
+
+
+	def SwitchProjection(self):
+		self.projection = self.ownerComp.par.Projection
+		self.ownerComp.par.Camera.enable = self.projection == 'Camera'
+		self.ownerComp.par.Offsetx.enable = not self.projection == 'Camera'
+
+
+	def formatFilepath(self):
+		filepath = str()
+
+		if self.folder:
+			filepath += self.folder + '/'
+
+		if str(self.filename).endswith('.svg'):
+			self.filename = str(self.filename)[:-4]
+		filepath += self.filename
+
+		if self.suffixe == 'Timestamp':
+			ts = time.time()
+			st = datetime.datetime.fromtimestamp(ts).strftime('%d%m%Y_%H%M%S')
+			filepath += '_' + st
+
+		filepath += '.svg'
+		self.filepath = filepath
+
+
+	def getPointsFromMeshSop(self, sop):
+		mesh = sop.prims[0]
+		polys = [x[:] for x in [[]] * (mesh.numCols + mesh.numRows)]
+		for i in range(mesh.numRows):
+			for j in range(mesh.numCols):
+				polys[i].append(self.parsePoint(mesh[i, j].point))
+				polys[mesh.numRows+j].append(self.parsePoint(mesh[i, j].point))
+		return polys
+
+
+	def getPointsFromPolySop(self, sop):
+		polys = []
+		for prim in sop.prims:
+			polys.append([self.parsePoint(vert.point) for vert in prim])
+		return polys
+
+
+	def parsePoint(self, point):
+		if self.projection == 'Offset':
+			return ((point.x + (point.z*self.offsetX)),
+						 	(point.y + (point.z*self.offsetY)))
+
+		elif self.projection == 'Camera':
+			pos = self.projectionMatrix * self.viewMatrix * point.P
+			return (pos[0], pos[1])
+
+
+	def scalePolysToFit(self, polys):
+		topLeft = numpy.min([numpy.min(poly, axis=0) for poly in polys], axis=(0))
+		bottomRight = numpy.max([numpy.max(poly, axis=0) for poly in polys], axis=(0))
+		width = bottomRight[0]-topLeft[0]
+		heigth = bottomRight[1]-topLeft[1]
+		scale = min((self.canvaW - self.margin*2) / width,
+								(self.canvaH - self.margin*2) / heigth)
+
+		for poly in polys:
+			for i in range(len(poly)):
+				poly[i] = ((poly[i][0]-topLeft[0])*scale + (self.canvaW-width*scale)/2,
+									 (poly[i][1]-topLeft[1])*scale + (self.canvaH-heigth*scale)/2)
+
+		return polys
+
+
+	def drawMesh(self, sop):
+		polys = self.getPointsFromMeshSop(sop)
+
+		if self.scaleToFit:
+			polys = self.scalePolysToFit(polys)
+
+		for poly in polys:
+			p = self.drawing.polyline(points=poly, stroke='black', stroke_width=1, fill='none')
+			self.drawing.add(p)
+
+
+	def drawPoly(self, sop):
+		polys = self.getPointsFromPolySop(sop)
+
+		if self.scaleToFit:
+			polys = self.scalePolysToFit(polys)
+
+		for poly in polys:
+			if self.polylineOnly:
+				p = self.drawing.polyline(points=poly, stroke='black', stroke_width=1, fill='none')
+			else:
+				p = self.drawing.polygon(points=poly, stroke='black', stroke_width=1, fill='none')
+			self.drawing.add(p)
+
+
+	def SaveSvg(self):
+		print('Saving SVG...')
+
+		self.loadParameters()
+		self.formatFilepath()
+
+		self.drawing = svgwrite.Drawing(
+			self.filepath,
+			size=('{}{}'.format(self.canvaW, self.unit), '{}{}'.format(self.canvaH, self.unit)),
+			viewBox=('0 0 {} {}'.format(self.canvaW, self.canvaH)))
+
+		if len(self.sop.prims) == 0:
+			print('Error: no primitive to draw')
+			return
+		elif isinstance(self.sop.prims[0], Mesh):
+			self.drawMesh(self.sop)
+		elif isinstance(self.sop.prims[0], Poly):
+			self.drawPoly(self.sop)
+		else:
+			print('Error: unsupported primitive type')
+			return
+
+		self.drawing.save()
+		print(self.filepath + ' saved')
